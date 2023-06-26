e539596ddf5462db282530d8d16bc1a307de9018
Samirsaiyed <samirsaiyed@gmail.com>
2023-06-26 13:37:18 +0530

    util file uploaded sucessfully

---
+++ housing/util/__init__.py
@@ -0,0 +1,13 @@
+import yaml
+from housing.exception import HousingException
+import os,sys
+
+def read_yaml_file(file_path:str)->dict:
+
+    try:
+        with open(file_path,'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise HousingException(e,sys) from e
+
+    
