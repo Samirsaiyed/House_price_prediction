859839cad768ce68ea9949ce99cdb0aaf812b969
Samirsaiyed <samirsaiyed@gmail.com>
2023-06-26 12:29:05 +0530

    updated

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def index():
     return "CI CD pipeline has been established."
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run()
---
+++ housing/entity/congif_entity.py
@@ -0,0 +1,20 @@
+from collections import namedtuple
+
+DataIngestionConfig = namedtuple("DataIngestionConfig",
+["dataset_download_url","tgz_download_dir","raw_data_dir","ingested_train_dir","ingested_test_dir"])
+
+DataValidationConfig = namedtuple("DataValidationConfig",
+["schema_file_path"])
+
+DataTransformationConfig = namedtuple("DataTransformationConfig",
+["add_badroom_per_room","transformed_train_dir","transformed_test_dir","preprocessed_onject_file_path"])
+
+
+ModelTranierConfig = namedtuple("ModelTranierConfig",
+["trained_model_file_path","base_accuracy"])
+
+ModelEvaluationConfig = namedtuple("ModelEvaluationConfig",
+["model_evaluation_file_path","time_stamp"])
+
+ModelPusherConfig = namedtuple("ModelPusherConfig",["export_dir_path"])
+
