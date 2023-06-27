208cea7a051a46ec1eeffadeab89e40de9200c60
Samirsaiyed <samirsaiyed@gmail.com>
2023-06-27 17:49:05 +0530

    read yaml file

--- housing/util/util.py
+++ housing/util/util.py
@@ -11,4 +11,4 @@ def read_yaml_file(file_path:str)->dict:
         with open(file_path, 'rb') as yaml_file:
             return yaml.safe_load(yaml_file)
     except Exception as e:
-        raise HousingException(e,sys) from e
+        raise HousingException(e,sys) from e    
