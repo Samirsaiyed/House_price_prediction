dac2f1f91a43e7d833ac167a8a44c5e8d1ba6608
Samirsaiyed <samirsaiyed@gmail.com>
2023-07-03 15:31:45 +0530

    updated.

---
+++ housing/entity/config_entity.py
@@ -0,0 +1,25 @@
+from collections import namedtuple
+
+# Provide the information of Data...............!!!!!
+
+DataIngestionConfig=namedtuple("DataIngestionConfig",
+["dataset_download_url","tgz_download_dir","raw_data_dir","ingested_train_dir","ingested_test_dir"])
+
+
+DataValidationConfig = namedtuple("DataValidationConfig", ["schema_file_path"])
+
+
+DataTransformationConfig = namedtuple("DataTransformationConfig", ["add_bedroom_per_room","transformed_train_dir","transformed_test_dir",
+"preprocessed_object_file_path"])
+
+
+ModelTrainerConfig = namedtuple("ModelTrainerConfig", ["trained_model_file_path","base_accuracy"])
+
+
+ModelEvaluationConfig = namedtuple("ModelEvaluationConfig", ["model_evaluation_file_path","time_stamp"])
+
+
+ModelPusherConfig = namedtuple("ModelPusherConfig", ["export_dir_path"])
+
+
+TrainingPipelineConfig = namedtuple("TrainingPipelineConfig", ["artifact_dir"])
